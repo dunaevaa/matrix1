43aed66903ee7c3df1a1b5ed0ade72cfadafb903
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-12-20 21:33:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(int argc, char* argv[]){
+int main (int argc, char* argv[]){
  int k, i;
  cin >> k;
  if (k <= 0){
      cout << "An error has occurred while reading input data" << endl;
      return 0; 
  }
- int *n = new int[k];
+ int *matrix = new int[k];
  for (i = 0; i < k; i++) {
-     cin >> n[i];
+     cin >> matrix[i];
  }
  for (i = 0; i < k / 2; i++) {
-     swap (n[i], n[k-i-1]);
+     swap (matrix[i], matrix[k-i-1]);
  }
  for (i = 0; i < k; i++) {
-     cout << n[i] << " ";
+     cout << matrix[i] << " ";
  }
- delete [] n;
+ delete [] matrix;
  return 0;
 }
